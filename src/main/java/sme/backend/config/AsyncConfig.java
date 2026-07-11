01500d3b73dc5ebc2066254e524451ef6f9188c6
lamnguyen <nhoklam2712005@gmail.com>
2026-07-11 16:52:12 +0700

    Version 2

---
+++ src/main/java/sme/backend/config/AsyncConfig.java
@@ -0,0 +1,26 @@
+package sme.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    @Primary
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("AsyncMail-");
+        executor.initialize();
+        return executor;
+    }
+}
